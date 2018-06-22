362b3a74e052e6375788c20e2634bbb342e0ff41
lukasz_huculak <lukasz.huculak@codete.com>
2018-06-22 13:41:32 +0200

    Setup example application

--- example/android/app/src/main/kotlin/com/fyber/flutter/fyber4flutterexample/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/fyber/flutter/fyber4flutterexample/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.fyber.flutter.fyber4flutterexample
 
 import android.os.Bundle
+import com.fyber.annotations.FyberSDK
 
 import io.flutter.app.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
+@FyberSDK
 class MainActivity(): FlutterActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
